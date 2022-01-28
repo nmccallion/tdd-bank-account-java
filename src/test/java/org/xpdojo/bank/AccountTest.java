4ef7f34b6d300c3ac029122f45a071a185c03503
niall <niall@jscl.uk>
2022-01-28 13:35:37 +0000

    got balance and deposit sorted

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -13,4 +13,19 @@ public class AccountTest {
         Account account = new Account();
         assertThat(account.balance()).isEqualTo(0);
     }
+
+    @Test
+    public void anAccount_putStuffInIt() {
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void anAccount_putMoreStuffIn() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
 }
